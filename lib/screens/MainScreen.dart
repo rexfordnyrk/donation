7ed916b89bc1d18c5f4152a57c63f7354c0c4332
Rexford Nyarko <rexfordnyrk@gmail.com>
2022-06-03 18:44:18 +0000

    Cleanup + initial Main Screen

---
+++ lib/screens/MainScreen.dart
@@ -0,0 +1,29 @@
+import 'package:donation/widgets/search_bar.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.white,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Icon(Icons.notifications,size: 25,color: Colors.blueGrey,),
+          )
+        ],
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+          ],
+        ),
+      ),
+    );
+  }
+}
