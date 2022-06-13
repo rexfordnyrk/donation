050bbd91fe26e13a911937047a42593e44963168
Rexford Nyarko <rexfordnyrk@gmail.com>
2022-06-13 09:39:09 +0000

    completed initial details screen

---
+++ lib/screens/DetailsScreen.dart
@@ -0,0 +1,77 @@
+import 'dart:ffi';
+
+import 'package:donation/widgets/detail_progress_card.dart';
+import 'package:donation/widgets/main_details.dart';
+import 'package:donation/widgets/organiser.dart';
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Icon(
+            Icons.arrow_back,
+            size: 25,
+            color: Colors.black,
+          ),
+        ),
+        title: const Align(
+          child: Text("Details",
+              textAlign: TextAlign.center,
+              style:
+              TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+        ),
+        backgroundColor: Colors.white,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Icon(
+              Icons.more_vert,
+              size: 25,
+              color: Colors.black,
+            ),
+          )
+        ],
+        elevation: 0,
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(left: 26.0, right: 26.0, top: 15),
+        color: Colors.transparent,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              MainDetails(),
+              DetailProgressCard(),
+              Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Organized by",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 19,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold))),
+              Organizer(),
+              MaterialButton(
+                onPressed: (){},
+                elevation: 2,
+                minWidth: double.maxFinite,
+                color: Colors.green,
+                child: const Padding(
+                  padding: EdgeInsets.all(18.0),
+                  child: Text("Donate Now", style: TextStyle(color: Colors.white),),
+                ),
+              )
+              // DonationItemCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
