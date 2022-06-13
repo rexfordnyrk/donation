f2b43801f0949bbed0a3e8a131e43f6e98127d6c
Rexford Nyarko <rexfordnyrk@gmail.com>
2022-06-13 14:39:41 +0000

    completed organizer widget

--- lib/widgets/organiser.dart
+++ lib/widgets/organiser.dart
@@ -5,55 +5,60 @@ class Organizer extends StatelessWidget{
   Widget build(BuildContext context) {
     return Container(
       padding:
-      const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+      const EdgeInsets.symmetric(vertical: 15),
       width: double.maxFinite,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width / 4,
-            margin: const EdgeInsets.only(right: 10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(5),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Container(
+              color: Colors.grey.withOpacity(0.2),
+              width: MediaQuery.of(context).size.width / 6,
+              padding: const EdgeInsets.all(10),
               child: Image.asset(
-                "assets/images/student_bag.jpg",
-                fit: BoxFit.fitWidth,
+                "assets/images/ic_eats_logo.png",
+                fit: BoxFit.contain,
               ),
             ),
           ),
           Flexible(
             flex: 1,
             fit: FlexFit.loose,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: const [
-                    Text(
-                      "Eats Foundation",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Icon(
-                        Icons.check_box_rounded,
-                        color: Colors.blue,
-                        size: 15,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        "Eats Foundation",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
                       ),
-                    )
-                  ],
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 8.0, bottom: 4),
-                  child: Text(
-                    "Verified Organizer",
-                    style:
-                    TextStyle(color: Colors.green, fontSize: 15, fontWeight: FontWeight.w500),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Icon(
+                          Icons.check_box_rounded,
+                          color: Colors.blue,
+                          size: 15,
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              ],
+                  const Padding(
+                    padding: EdgeInsets.only(top: 8.0, bottom: 4),
+                    child: Text(
+                      "Verified Organizer",
+                      style:
+                      TextStyle(color: Colors.green, fontSize: 15, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         ],
